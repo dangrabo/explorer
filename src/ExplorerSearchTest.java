fd74f9cf203900adc95e6333c6047f65badb1961
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-06-01 15:30:48 -0700

    added tests for explorerLocation

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -15,6 +15,66 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
-    // Add more tests here!
-    // Come up with varied cases
+    // Test explorerLocation
+    @Test
+    public void testExplorerLocation_validLocation() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,0,1},
+            {1,1,1,2,1,1},
+        };
+        int[] actual = ExplorerSearch.explorerLocation(island);
+        assertEquals(3, actual[0]);
+        assertEquals(4, actual[1]);
+    }
+
+    @Test
+    public void testExplorerLocation_startingLocation() {
+        int[][] island = {
+            {0,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,1},
+        };
+        int[] actual = ExplorerSearch.explorerLocation(island);
+        assertEquals(0, actual[0]);
+        assertEquals(0, actual[1]);
+    }
+
+    @Test
+    public void testExplorerLocation_endingLocation() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,0},
+        };
+        int[] actual = ExplorerSearch.explorerLocation(island);
+        assertEquals(4, actual[0]);
+        assertEquals(5, actual[1]);
+    }
+
+    @Test
+    public void testExplorerLocation_invalidLocation() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,1},
+        };
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class, () -> { ExplorerSearch.explorerLocation(island); }
+            );
+    
+        assertEquals("No explorer present", exception.getMessage());
+    }
+
+    // Test possibleMoves
+    
 }
