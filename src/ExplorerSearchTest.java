314a5c49e9987497ff45da0a624076669d7c4d29
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-06-01 16:10:27 -0700

    Added tests for reachableArea

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -16,6 +16,45 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
+    @Test
+    public void testReachableArea_allReachable() {
+        int[][] island = {
+            {2,3,2,3,2,3},
+            {3,2,3,1,3,2},
+            {1,1,1,1,3,3},
+            {3,1,2,1,0,1},
+            {1,1,1,2,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(14, actual);
+    }
+
+    @Test
+    public void testReachableArea_allPossibleReachable() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,0,1},
+            {1,1,1,1,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(30, actual);
+    }
+
+    @Test
+    public void testReachableArea_noneReachable() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,2,0,2},
+            {1,1,1,2,3,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
     // Test explorerLocation
     @Test
     public void testExplorerLocation_validLocation() {
